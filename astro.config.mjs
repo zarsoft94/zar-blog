75a733cf229f9e08ebfa5f69da66c54e44d13c0a
Zari <zaritaghipoor85@gmail.com>
2024-05-20 13:22:03 +0330

    added RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zar-blog.pages.dev',
   output: "server",
   adapter: cloudflare()
 });
