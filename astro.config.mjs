73faf4a993af2237df35d9daaef0605e801704dc
Zari <zaritaghipoor85@gmail.com>
2024-05-28 12:34:47 +0330

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zar-blog.pages.dev',
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [preact()]
 });
